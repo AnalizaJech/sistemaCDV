481baaee974375a80ec12b98947e6c0f5260eab9
Analiza Jech <2101010055@undc.edu.pe>
2023-08-12 22:28:32 -0500

    Primeros recursos y conexion a php

---
+++ conexion.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$database = "cdv";
+$username = "root";
+$password = "";
+// Crea la conexion
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Chomprueba la conexion
+/*
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+mysqli_close($conn);
+*/
+?>
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+require 'conexion.php';
+
+/*
+Podemos pensar en utilizar include cuando el archivo a introducir 
+no sea determinante respecto al funcionamiento de nuestro programa. 
+Require cuando dicho archivo sea necesario para el correcto 
+funcionamiento de nuestro programa.
+
+Finalmente se debe utilizar las variantes con _once cuando nuestro 
+programa tenga unas dimensiones considerables y pueda darse el caso 
+de que la inclusión del fichero se produzca varias veces. Estas últimas 
+variantes hay que usarlas solo en casos excepcionales ya que consumen 
+más recursos que las anteriores. 
+*/
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Principal</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="assets/img/cdv.png">
+</head>
+<body>
+    
+</body>
+</html>
